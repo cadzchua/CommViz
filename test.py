8784a0e0ab23cf6949640fbb07de2d08462fa0ab
cadzchua <cadidic3h@gmail.com>
2024-04-26 05:51:54 +0800

    feat: testing streamlit with new UI and email analysis only

---
+++ test.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import networkx as nx
+from pyvis.network import Network
+import re, os
+
+def extract_emails(text):
+    match = re.search(r'(.+@[A-Za-z]+.com)', str(text))
+    if match:
+        return match.group(1)
+    else:
+        return None
+    
+uploaded_file = st.sidebar.file_uploader("Upload your input xls file", type=["xls", "xlsx"])
+if uploaded_file is not None:
+    xls = pd.ExcelFile(uploaded_file)
+    device_info = xls.parse("Device Information", header=1)
+    device_info['Name'] = device_info['Name'].str.strip()
+    android_id = device_info[device_info["Name"] == "Android ID"]['Value'] # Extracted User Device ID
+    emails = xls.parse("Emails", header=1)
+    emails['Email (From:)'] = emails['From'].apply(extract_emails)
+    emails['Email (To:)'] = emails['To'].apply(extract_emails)
+    emails.dropna(subset=['Email (From:)', 'Email (To:)'], how='all', inplace=True)
+    emails['Email (From:)'].fillna(android_id.iloc[0], inplace=True)
+    emails['Email (To:)'].fillna(android_id.iloc[0], inplace=True)
+    all_emails = set(emails['Email (To:)'].dropna()) | set(emails['Email (From:)'].dropna())
+    emails['to_from_tuple'] = list(zip(emails['Email (From:)'], emails['Email (To:)']))
+    tuple_counts3 = emails['to_from_tuple'].value_counts()
+    df_email = pd.DataFrame(columns=['from', 'to', 'weight'])  # Initialize an empty DataFrame
+    for (from_num, to_num), count in tuple_counts3.items():
+        df_email.loc[len(df_email)] = [from_num, to_num, count**0.5]
+
+    # Set header title
+    st.title('Network Graph Visualization')
+    
+    # Define list of selection options and sort alphabetically
+    list = ['Call', 'Instant Messages', 'Email']
+    list.sort()
+
+    # Implement multiselect dropdown menu for option selection (returns a list)
+    selected_options = st.multiselect('Select option(s) to visualize', list)
+
+    heading_text = ", ".join(selected_options) + " Network"
+    st.header(heading_text)
+    # Set info message on initial site load
+    if len(selected_options) == 0:
+        st.text('Choose at least 1 option to start')
+
+    # Create network graph when user selects >= 1 item
+    else:
+       if "Email" in selected_options:
+        # Create networkx graph object from pandas dataframe
+            G_email = nx.from_pandas_edgelist(df_email, 'from', 'to', 'weight')
+            
+            # Initiate PyVis network object
+            email_net = Network(
+                            height='450px',
+                            width='100%',
+                            bgcolor='#121925',
+                            font_color='white',
+                            directed=True
+                            )
+
+            # Take Networkx graph and translate it to a PyVis graph format
+            email_net.from_nx(G_email)
+
+            # Generate network with specific layout settings
+            email_net.repulsion(
+                                node_distance=420,
+                                central_gravity=0.33,
+                                spring_length=110,
+                                spring_strength=0.10,
+                                damping=0.95
+                            )
+
+            # Save and read graph as HTML file (on Streamlit Sharing)
+            
+            path = 'tmp'
+            if not os.path.exists(path):
+                os.makedirs(path)
+            email_net.save_graph(f'{path}/pyvis_graph.html')
+            HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+            # Load HTML file in HTML component for display on Streamlit page
+            components.html(HtmlFile.read(), height=470)
